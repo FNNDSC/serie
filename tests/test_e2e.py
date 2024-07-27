c2f31db8b880cb496dafe74a431ecc52e6bf04fa
Jennings Zhang <dev@sl.jennin.xyz>
2024-07-27 17:16:42 -0400

    Reorganize test helper functions

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,13 +1,9 @@
 import asyncio
 import textwrap
-from pathlib import Path
 
 import docker
-import pydicom
-import pynetdicom
 import pytest
 import pytest_asyncio
-import requests
 import uvicorn
 from aiochris import ChrisClient
 from aiochris.errors import IncorrectLoginError
@@ -17,6 +13,7 @@ from fastapi import FastAPI
 import tests.e2e_config as config
 from serie import router
 from serie.settings import get_settings
+from tests.helpers import download_and_send_dicom
 from tests.uvicorn_test_server import UvicornTestServer
 
 
@@ -76,10 +73,10 @@ async def chris() -> ChrisClient:
 
 
 async def _start_test(chris: ChrisClient):
-    await asyncify(clear_dicoms_from_cube)()
+    await asyncify(clear_dicoms_from_cube)(config.AE_TITLE)
     assert await chris.search_pacsfiles(pacs_identifier=config.AE_TITLE).count() == 0
 
-    await asyncify(send_sample_dicom)()
+    await asyncify(download_and_send_dicom)(config.EXAMPLE_DOWNLOAD_URL, config.AE_TITLE)
 
     elapsed = 0
     while await chris.search_pacsfiles(pacs_identifier=config.AE_TITLE).count() == 0:
@@ -91,7 +88,7 @@ async def _start_test(chris: ChrisClient):
             )
 
 
-def clear_dicoms_from_cube():
+def clear_dicoms_from_cube(ae_title: str):
     client = docker.from_env()
     cube = client.containers.get(config.CUBE_CONTAINER_ID)
     cube.exec_run(
@@ -103,7 +100,7 @@ def clear_dicoms_from_cube():
             textwrap.dedent(f"""
         from pacsfiles.models import PACS, PACSFile
         try:
-            pacs = PACS.objects.get(identifier='{config.AE_TITLE}')
+            pacs = PACS.objects.get(identifier='{ae_title}')
             for pacs_file in PACSFile.objects.filter(pacs=pacs):
                 pacs_file.delete()
         except PACS.DoesNotExist:
@@ -111,38 +108,3 @@ def clear_dicoms_from_cube():
         """),
         ]
     )
-
-
-def send_sample_dicom():
-    """
-    See https://pydicom.github.io/pynetdicom/stable/examples/storage.html#storage-scu
-    """
-
-    ds = pydicom.dcmread(get_sample_dicom())
-    ae = pynetdicom.AE(ae_title="SERIETEST")
-    ae.add_requested_context(ds.file_meta.MediaStorageSOPClassUID)
-    assoc = ae.associate(
-        config.OXIDICOM_HOST, config.OXIDICOM_PORT, ae_title=config.OXIDICOM_AET
-    )
-    if not assoc.is_established:
-        raise pytest.fail("Could not establish association with oxidicom")
-    status = assoc.send_c_store(ds)
-    if not status:
-        raise pytest.fail(
-            "Failed to send data to oxidicom: "
-            "connection timed out, was aborted or received invalid response"
-        )
-    assoc.release()
-
-
-def get_sample_dicom() -> Path:
-    tmp_dicom = Path(__file__).parent.parent / ".test_data" / "chest_ct.dcm"
-    if tmp_dicom.exists():
-        return tmp_dicom
-    tmp_dicom.parent.mkdir(parents=True, exist_ok=True)
-    with requests.get(config.DOWNLOAD_URL, stream=True) as r:
-        r.raise_for_status()
-        with tmp_dicom.open("wb") as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-    return tmp_dicom
